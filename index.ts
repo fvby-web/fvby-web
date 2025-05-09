22b4c534bb4540d7fe4b0bcfa4e6cc165a95fc72
fvby-web <kratos.roman456@gmail.com>
2025-05-08 22:07:12 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
+import { connectToDatabase } from "./db";
+
+async function main() {
+  const db = await connectToDatabase();
+  const usuarios = await db.collection("usuarios").find().toArray();
+  console.log("Usuarios:", usuarios);
+}
+
+main();
 
