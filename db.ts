7d0e5d14f71dd00a1d9ccf20b1ddc1dafee936ca
fvby-web <kratos.roman456@gmail.com>
2025-05-08 22:03:47 -0400

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,1 +1,17 @@
+import { MongoClient } from "mongodb";
+
+const uri = "mongodb+srv://kratosroman456:Hola123@cluster0.thxlr.mongodb.net/"; // Cambia por tu URI
+const client = new MongoClient(uri);
+
+export async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Conectado a MongoDB");
+    const db = client.db("mi_base_de_datos"); // Cambia por el nombre de tu base
+    return db;
+  } catch (error) {
+    console.error("Error al conectar a MongoDB", error);
+    throw error;
+  }
+}
 
